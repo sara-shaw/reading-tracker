04718815bb9ddfd5c8a6ed39afd98a5ef6cea152
Sara <sara.667@outlook.com>
2023-07-31 19:38:26 -0600

    components

--- reading-tracker/src/app/app.module.ts
+++ reading-tracker/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { CurrentlyReadingComponent } from './currently-reading/currently-reading.component';
+import { WantToReadComponent } from './want-to-read/want-to-read.component';
+import { ReadComponent } from './read/read.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthComponent,
+    CurrentlyReadingComponent,
+    WantToReadComponent,
+    ReadComponent
   ],
   imports: [
     BrowserModule
---
+++ reading-tracker/src/app/want-to-read/want-to-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-want-to-read',
+  templateUrl: './want-to-read.component.html',
+  styleUrls: ['./want-to-read.component.css']
+})
+export class WantToReadComponent {
+
+}
